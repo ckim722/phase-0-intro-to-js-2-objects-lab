7889310f26fcf21a0c2f6856d8cb3976935dc6dc
Catherine Kim <ckim722@gmail.com>
2023-01-21 14:38:11 -0500

    edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "nomer",
+    streetAddress: "value"
+};
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    //take in 3 arguments: employee Object, a key, a value
+    //shouldn't mutate employee;return new Object w/updated value for key passed in.
+    //Hint: use spread operator!
+    return{
+        ...employee,
+        [streetAddress]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    employee[streetAddress]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    delete employee.name;
+    return employee;
+}
